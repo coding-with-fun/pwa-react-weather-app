dbc2f2071ed9952321fde7f150cedc9829d2b56c
Harsh Patel <harshp2482@gmail.com>
2020-07-20 17:00:29 +0530

    Minor tweaks

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
 
 	const search = async (e) => {
 		if (e.key === "Enter") {
-			setLoading(true);
 			setQuery("");
-			const data = await FetchData(query);
-			setWeather(data);
+			setLoading(true);
+			let data = "";
+			try {
+				data = await FetchData(query);
+			} catch (error) {
+				console.log(error);
+			}
 			setLoading(false);
+			setWeather(data);
 		}
 	};
 
@@ -28,50 +33,34 @@ function App() {
 				onKeyPress={search}
 			/>
 
-			{loading ? (
+			{loading && (
 				<div className="city">
 					<h2>Loading...</h2>
 				</div>
-			) : (
-				<>
-					{weather.main && (
-						<div className="city">
-							<h2 className="city-name">
-								<span>{weather.name}</span>
-								<sup>{weather.sys.country}</sup>
-							</h2>
-
-							<div className="city-temp">
-								{Math.round(weather.main.temp)}
-								<sup>&deg;C</sup>
-							</div>
-
-							<div className="info">
-								<img
-									src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-									alt={weather.weather[0].description}
-									className="city-icon"
-								/>
-								<p>{weather.weather[0].description}</p>
-							</div>
-						</div>
-					)}
-				</>
 			)}
 
-			<footer className="footer">
-				<div className="container">
-					<span className="text-muted">
-						© 2020 Copyright:
-						<a
-							href="https://github.com/harsh2124/pwa-react-weather-app"
-							target="blank"
-						>
-							<b> Harsh Patel</b>
-						</a>
-					</span>
+			{weather.main && (
+				<div className="city">
+					<h2 className="city-name">
+						<span>{weather.name}</span>
+						<sup>{weather.sys.country}</sup>
+					</h2>
+
+					<div className="city-temp">
+						{Math.round(weather.main.temp)}
+						<sup>&deg;C</sup>
+					</div>
+
+					<div className="info">
+						<img
+							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+							alt={weather.weather[0].description}
+							className="city-icon"
+						/>
+						<p>{weather.weather[0].description}</p>
+					</div>
 				</div>
-			</footer>
+			)}
 		</div>
 	);
 }
