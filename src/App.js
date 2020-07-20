65cf288e28e4e636ba78f8237f3809d367b84244
Harsh Patel <harshp2482@gmail.com>
2020-07-20 14:21:49 +0530

    Added footer and loading screen

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import "./App.css";
 function App() {
 	const [query, setQuery] = useState("");
 	const [weather, setWeather] = useState("");
+	const [loading, setLoading] = useState(false);
 
 	const search = async (e) => {
 		if (e.key === "Enter") {
+			setLoading(true);
+			setQuery("");
 			const data = await FetchData(query);
 			setWeather(data);
-			setQuery("");
+			setLoading(false);
 		}
 	};
 
@@ -25,28 +28,47 @@ function App() {
 				onKeyPress={search}
 			/>
 
-			{weather.main && (
+			{loading ? (
 				<div className="city">
-					<h2 className="city-name">
-						<span>{weather.name}</span>
-						<sup>{weather.sys.country}</sup>
-					</h2>
-
-					<div className="city-temp">
-						{Math.round(weather.main.temp)}
-						<sup>&deg;C</sup>
-					</div>
-
-					<div className="info">
-						<img
-							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-							alt={weather.weather[0].description}
-							className="city-icon"
-						/>
-						<p>{weather.weather[0].description}</p>
-					</div>
+					<h2>Loading...</h2>
 				</div>
+			) : (
+				<>
+					{weather.main && (
+						<div className="city">
+							<h2 className="city-name">
+								<span>{weather.name}</span>
+								<sup>{weather.sys.country}</sup>
+							</h2>
+
+							<div className="city-temp">
+								{Math.round(weather.main.temp)}
+								<sup>&deg;C</sup>
+							</div>
+
+							<div className="info">
+								<img
+									src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+									alt={weather.weather[0].description}
+									className="city-icon"
+								/>
+								<p>{weather.weather[0].description}</p>
+							</div>
+						</div>
+					)}
+				</>
 			)}
+
+			<footer className="footer">
+				<div className="container">
+					<span className="text-muted">
+						© 2020 Copyright:
+						<a href="https://github.com/harsh2124/blog-app" target="blank">
+							<b> Harsh Patel</b>
+						</a>
+					</span>
+				</div>
+			</footer>
 		</div>
 	);
 }
